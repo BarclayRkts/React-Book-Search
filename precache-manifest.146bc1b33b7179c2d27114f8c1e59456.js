1101de12f59684734ffd3382e9760880901bfe11
DeJa <barclay.deja@gmail.com>
2020-07-10 17:01:41 -0500

    Updates

---
+++ precache-manifest.146bc1b33b7179c2d27114f8c1e59456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352263413cda92ae53f579c14e819ef2",
+    "url": "/React-Book-Search/index.html"
+  },
+  {
+    "revision": "4a98e2d2da49a110eb92",
+    "url": "/React-Book-Search/static/css/main.f19c4012.chunk.css"
+  },
+  {
+    "revision": "8c00e57f48203d392a0b",
+    "url": "/React-Book-Search/static/js/2.1f138b9c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Book-Search/static/js/2.1f138b9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a98e2d2da49a110eb92",
+    "url": "/React-Book-Search/static/js/main.21d36853.chunk.js"
+  },
+  {
+    "revision": "b5a3ad994b8e1caea497",
+    "url": "/React-Book-Search/static/js/runtime-main.9e16add0.js"
+  }
+]);
